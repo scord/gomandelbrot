b80fcafe52f67b4b696e3023b5bc080746483405
Sam Cordingley <samcordingley@gmail.com>
2014-07-09 04:19:18 +0100

    change pixels to tiles

--- mandelbrot.go
+++ mandelbrot.go
@@ -7,19 +7,22 @@ import (
 	"math/rand"
 	"os"
 	"runtime"
+	"sync"
 )
 
-type pixel struct {
-	x, y int
+type tile struct {
+	x1, x2, y1, y2 int
 }
 
 func mandelbrot(w, h, i int, z float32, seed int64) *image.RGBA {
 
-	work := make(chan pixel)
+	work := make(chan tile)
 
 	colors := make([]color.RGBA, i)
 
 	rand.Seed(seed)
+
+	var wg = new(sync.WaitGroup)
 
 	for index := range colors {
 		colors[index] = randomColor()
@@ -29,24 +32,35 @@ func mandelbrot(w, h, i int, z float32, seed int64) *image.RGBA {
 
 	m := image.NewRGBA(image.Rect(0, 0, w, h))
 
-	for t := 0; t < 256; t++ {
+	for t := 0; t < runtime.NumCPU(); t++ {
 		go func() {
-			for p := range work {
-				setColor(m, colors, p.x, p.y, i, zoom)
+			for tile := range work {
+				for x := tile.x1; x < tile.x2; x++ {
+					for y := tile.y1; y < tile.y2; y++ {
+						setColor(m, colors, x, y, i, zoom)
+					}
+				}
+				wg.Done()
 			}
 		}()
 	}
 
-	b := m.Bounds()
+	tx := 1
+	ty := h
 
-	for y := b.Min.Y; y < b.Max.Y; y++ {
-		for x := b.Min.X; x < b.Max.X; x++ {
-			work <- pixel{x, y}
+	wg.Add(tx * ty)
+
+	go func() {
+		for x := 0; x < w; x += w / tx {
+			for y := 0; y < h; y += h / ty {
+				work <- tile{x, x + w/tx, y, y + h/ty}
+			}
 		}
-	}
 
-	close(work)
+		close(work)
+	}()
 
+	wg.Wait()
 	return m
 
 }
@@ -86,9 +100,8 @@ func setColor(m *image.RGBA, colors []color.RGBA, px, py, maxi int, zoom float32
 }
 
 func main() {
-
 	runtime.GOMAXPROCS(runtime.NumCPU())
-	m := mandelbrot(1000, 1000, 100, 1.0, 1)
+	m := mandelbrot(1000, 1000, 1000, 1.0, 346)
 
 	w, _ := os.Create("mandelbrot.png")
 	defer w.Close()
